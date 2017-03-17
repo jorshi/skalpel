e5d3cde08f0e2791324d2918cdee81262e066fbc
jorshi <jordieshier@gmail.com>
2017-03-17 13:22:43 -0700

    New helper class for loading audio files

---
+++ Source/FileLoader.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    FileLoader.h
+    Created: 17 Mar 2017 1:20:56pm
+    Author:  Jordie Shier 
+
+  ==============================================================================
+*/
+
+#ifndef FILELOADER_H_INCLUDED
+#define FILELOADER_H_INCLUDED
+
+
+
+
+
+#endif  // FILELOADER_H_INCLUDED
